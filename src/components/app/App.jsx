c3dc368ec6949fb3231d86db2efd2df5ad71f1b9
Jena Boehm <boehmjena@gmail.com>
2021-04-08 15:12:36 -0700

    add colors and more styling

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -3,7 +3,8 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import styles from './App.module.css';
 import whitney from '../../../public/whitney.jpeg'
-import Header from '../header/Header';
+import alchemyLogo from '../../../public/alchemy-logo-white-bg.png'
+// import Header from '../header/Header';
 
 AOS.init({ duration: 800 })
 
@@ -12,48 +13,57 @@ export default class App extends Component {
   render() {
     return (
       <div className={styles.root}>
-        <Header />
+        {/* <Header /> */}
         <div data-aos="fade-down" className={styles.home}>
-          <h1>Thinking of You</h1>
+          <div className={styles.title}>Thinking of You</div>
           <div className={styles.frame}>
             <img className={styles.whitneyImage} src={whitney} alt='whitney' />
           </div>
-          <p>Whitney, you've been on our minds. We wanted to send you some smiles and encouragement to get you through this difficult time.</p>
+          <div className={styles.description}>
+            <p>Whitney, you've been on our minds. We wanted to send you some smiles and encouragement to get you through this difficult time.</p>
 
-          <p>Why do we use it?
-          It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+            <p>Why do we use it?
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
           </p>
+          </div>
         </div>
-
-        <div className={styles.message} data-aos="zoom in" data-aos-anchor=".other-element">
-          <p>
-            Hello, this is an anchor from another element
+        <div className={styles.messagesContainer}>
+          <div className={styles.messageLeft} data-aos="fade-right" data-aos-anchor=".other-element">
+            <h3>From: (name)</h3>
+            <p>
+              It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
           </p>
-        </div>
-        <div className={styles.message} data-aos="fade-up">
-          <p>
-            This is an example of one message.
+          </div>
+          <div className={styles.messageRight} data-aos="fade-left">
+            <h3>From: (name)</h3>
+            <p>
+              It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
           </p>
-        </div>
-        <div className={styles.message} data-aos="fade-up">
-          <p>
-            This is an example of one message.
+          </div>
+          <div className={styles.messageLeft} data-aos="fade-right">
+            <h3>From: (name)</h3>
+            <p>
+              It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
           </p>
-        </div>
-        <div className={styles.message} data-aos="fade-down-right">
-          <p>
-            This is an example of one message.
+          </div>
+          <div className={styles.messageRight} data-aos="fade-left">
+            <h3>From: (name)</h3>
+            <p>
+              It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
           </p>
-        </div>
-        <div className={styles.message} data-aos="zoom in">
-          <p>
-            This is an example of one message.
+          </div>
+          <div className={styles.messageLeft} data-aos="fade-right">
+            <h3>From: (name)</h3>
+            <p>
+              It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
           </p>
-        </div>
-        <div className={styles.message} data-aos="fade-down-left">
-          <p>
-            This is an example of one message.
+          </div>
+          <div className={styles.messageRight} data-aos="fade-left">
+            <h3>From: (name)</h3>
+            <p>
+              It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
           </p>
+          </div>
         </div>
       </div>
     )
