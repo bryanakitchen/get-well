68fa9d81c3c1245321acf3848371e495b7fd13a0
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-04-14 13:11:41 -0400

    another message

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -62,28 +62,29 @@ export default class App extends Component {
             </p>
           </div>
 
+          <div className={styles.messageCenter} data-aos="fade-left">
+            <h3>From: Katie Perry</h3>
+            <img src='../../../whitney-card.png' alt="card for Whitney" className={styles.card} />
+          </div>
+
+          <div className={styles.messageRight} data-aos="fade-right">
+            <h3>From: Dani Cairns</h3>
+            <p>
+              Whitney,
+            </p>
+            <p>
+              We've all been missing your energetic and talented presence :) Looking forward to partying with you at Alchemy headquarters once Alchemy the school is back in real life! You're the best!
+            </p>
+          </div>
+
           <div className={styles.messageLeft} data-aos="fade-right">
             <h3>From: Marty Nelson</h3>
             <p>
               Take your sweet time getting well! Here's to you—steadier, stronger and better every day.
             </p>
           </div>
-          <div className={styles.messageCenter} data-aos="fade-left">
-            <h3>From: Katie Perry</h3>
-            <img src='../../../whitney-card.png' alt="card for Whitney" className={styles.card} />
-          </div>
 
-          <div className={styles.messageLeft} data-aos="fade-right">
-            <h3>From: (hello)</h3>
-            <p>
-              Message goes here
-            </p>
-            <p>
-              optional signature
-            </p>
-          </div>
-
-          <div className={styles.messageRight} data-aos="fade-left">
+          {/* <div className={styles.messageRight} data-aos="fade-left">
             <h3>From: (name)</h3>
             <p>
               Message goes here
@@ -91,7 +92,7 @@ export default class App extends Component {
             <p>
               optional signature
             </p>
-          </div>
+          </div> */}
         </div>
       </div>
     )
